a0edad90658801018324c0bd278e0000c615f902
b8goal <b8goal@naver.com>
2020-03-21 17:44:05 +0900

    Add Subject, Toc, Content

--- src/App.js
+++ src/App.js
@@ -2,11 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Subject extends Component {
+  render(){
+    return(
+      <header>
+        <h1>Web</h1>
+        world wide web
+      </header>
+    );
+  }
+}
+
+class Toc extends Component {
+  render() {
+    return(
+      <nav>
+        <li><a href="1.html">Web</a></li>
+        <li><a href="2.html">CSS</a></li>
+        <li><a href="3.html">JavaScript</a></li>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return(
+      <article>
+        <h2>Web</h2>
+        World Wide Web
+      </article>
+    );
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello React
+        <Subject></Subject>
+        <Toc></Toc>
+        <Content></Content>
       </div>
     );
   }
