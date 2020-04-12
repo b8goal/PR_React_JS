3b2eca25ebe97c69b57ef6bc03d2c2caae4d0764
HyeonSeong-Bae <HyeonSeong-Bae@stradvision.com>
2020-04-12 17:04:26 +0900

    Divide to component files
    import React, { Component } from 'react';
    react라는 lib에서 Component Class를 사용함을 의미
    react를 사용하기 때문에 import React, 는 필수적으로 포함되어야함

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React, { Component } from 'react';
+import Subject from "./components/Subject"
+import TOC from "./components/TOC"
+import Content from "./components/Content"
 import logo from './logo.svg';
 import './App.css';
 
-class Subject extends Component {
-  render(){
-    return(
-      <header>
-        <h1>Web</h1>
-        world wide web
-      </header>
-    );
-  }
-}
-
-class Toc extends Component {
-  render() {
-    return(
-      <nav>
-        <li><a href="1.html">Web</a></li>
-        <li><a href="2.html">CSS</a></li>
-        <li><a href="3.html">JavaScript</a></li>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return(
-      <article>
-        <h2>Web</h2>
-        World Wide Web
-      </article>
-    );
-  }
-}
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Subject></Subject>
-        <Toc></Toc>
+        <TOC></TOC>
         <Content></Content>
       </div>
     );
---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Content extends Component {
+  render() {
+    return(
+      <article>
+        <h2>Web</h2>
+        World Wide Web
+      </article>
+    );
+  }
+}
+
+export default Content;
