dec9fa0d0e0085b996c15ca17ac1b8149106f8e0
HyeonSeong-Bae <HyeonSeong-Bae@stradvision.com>
2020-04-12 19:13:09 +0900

    Make component event

--- src/App.js
+++ src/App.js
@@ -38,7 +38,10 @@ class App extends Component {
           }.bind(this)}
         >
         </Subject>
-        <TOC data={this.state.contents}></TOC>
+        <TOC onChangePage={function(){
+          this.setState({mode:'read'});
+        }.bind(this)}
+        data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
